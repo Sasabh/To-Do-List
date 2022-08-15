2ba0097711f976ac00aed48cade48fe34d5ae3f9
Sasabh Gupta <blaze>
2022-08-16 05:18:12 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,38 @@
-import { Container } from '@mui/system';
-import { Component, Fragment } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import TaskTracker from './components/TaskTracker';
-import WebHeader from './components/WebHeader';
-import Footer from './components/Footer';
-import About from './components/About';
+import { Container } from "@mui/system";
+import { Component, Fragment } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
+import TaskTracker from "./components/TaskTracker";
+import WebHeader from "./components/WebHeader";
+import Footer from "./components/Footer";
+import About from "./components/About";
 
 class App extends Component {
   name = "Noran";
-  x = 10
+  x = 10;
 
   render() {
     return (
       <Router>
         <Fragment>
-          <WebHeader title="To-Do List" style={{display: "block"}}></WebHeader>
-          <Container maxWidth="xl" style={{marginTop: "2rem"}}>
+          <WebHeader
+            title="To-Do List"
+            style={{ display: "block" }}
+          ></WebHeader>
+          <Container maxWidth="xl" style={{ marginTop: "2rem" }}>
             <Routes>
-              <Route path='/' exact element={<TaskTracker />}></Route>
-              <Route path='/about' element={<About />}></Route>
+              <Route
+                path="/"
+                exact
+                element={<Navigate to="/to-do-list" replace />}
+              ></Route>
+              <Route path="/to-do-list" exact element={<TaskTracker />}></Route>
+              <Route path="/to-do-list/about" element={<About />}></Route>
             </Routes>
             <Footer></Footer>
           </Container>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer>
       <p>Copyright &copy; 2022</p>
-      <Link to="/about">About</Link>
+      <Link to="/to-do-list/about">About</Link>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
